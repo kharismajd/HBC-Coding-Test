6cf226c6d417883632293a0f1deacbd5aa5e7d45
= <kharismajagaddhita170501@gmail.com>
2026-04-25 20:40:02 +0700

    [HBC-1] Initialize Project with Spring Initializr

---
+++ src/test/java/com/harebusiness/form/FormServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harebusiness.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
