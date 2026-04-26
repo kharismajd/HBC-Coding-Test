dd03e467f71174e2f96e1519bae0920df416d86b
= <kharismajagaddhita170501@gmail.com>
2026-04-26 12:11:52 +0700

    [HBC-3] Created repositories for each models

---
+++ src/main/java/com/harebusiness/form/repositories/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.harebusiness.form.repositories;
+
+import com.harebusiness.form.models.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
---
+++ src/main/java/com/harebusiness/form/repositories/FormRepository.java
@@ -0,0 +1,10 @@
+package com.harebusiness.form.repositories;
+
+import com.harebusiness.form.models.Form;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormRepository extends JpaRepository<Form, Long> {
+
+}
